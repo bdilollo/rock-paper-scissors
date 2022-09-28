521cf91f5895bb454747f0389f8e4de2cce6aa08
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-09-28 14:11:48 -0500

    create html and pseudocode script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// initial prompt to enter R, P, or S
+// store the response as a variable
+// re-prompt if input isn't R, P, or S
+
+// Computer should generate random R, P, or S and store as variable
+
+// Compare user input value with random generated value
+    // if equal, alert indicating a tie
+    // if different, determine winner
+    // S beats P, P beats R, R beats S
+    // log result (W/L/T)
+
+// alert saying 'won', 'lost', or 'tie'
+// alert displaying tally of wins, losses, ties
+// ask "play again?"
+    // if yes, return to initial prompt, start process over
+
+console.log("hello");
